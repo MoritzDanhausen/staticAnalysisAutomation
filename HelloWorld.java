f4ae5abc77c5048790c9d8ecd66fcc4a8b2b7070
MoritzDanhausen <76939957+MoritzDanhausen@users.noreply.github.com>
2022-01-02 12:27:21 +0100

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hallo Welt");
+		System.out.println("Hallo Welt2");
+		System.out.println("Hallo Welt3");
+		System.out.println("These were online changes");
+	}
+
+}
